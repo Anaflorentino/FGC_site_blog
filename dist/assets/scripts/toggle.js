f19f6250770aa3fd4bcd7ed587621a2abdbc1806
Ana Florentino <abmflorentino@gmail.com>
2024-12-16 15:10:29 -0300

    Adjusts on Hero Section

--- dist/assets/scripts/toggle.js
+++ dist/assets/scripts/toggle.js
@@ -1,5 +1,17 @@
-const toggleInput = document.querySelector('.toggle-input');
+ // Adiciona a funcionalidade do toggle
+ const toggle = document.getElementById('pet-friendly-toggle');
+ const toggleText = document.getElementById('pet-friendly-text');
+ 
+ // Função para alternar o estado do toggle
+ const toggleState = () => {
+     toggle.classList.toggle('active');
+ };
 
-toggleInput.addEventListener('change', function() {
-  console.log('Pet friendly is now:', this.checked ? 'On' : 'Off');
-});
+ // Evento de clique no toggle
+ toggle.addEventListener('click', toggleState);
+
+ // Evento de clique no texto "Pet friendly"
+ toggleText.addEventListener('click', toggleState);
+
+
+ 
