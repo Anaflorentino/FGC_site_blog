3ffe854ef52cf60b0f70a69730b04f055f91fdb7
oBregadiolli <bregadiolli.contato@gmail.com>
2024-12-23 16:34:28 -0300

    Pre-Form

--- dist/assets/scripts/toggle.js
+++ dist/assets/scripts/toggle.js
@@ -1,17 +1,28 @@
- // Adiciona a funcionalidade do toggle
- const toggle = document.getElementById('pet-friendly-toggle');
- const toggleText = document.getElementById('pet-friendly-text');
- 
- // Função para alternar o estado do toggle
- const toggleState = () => {
-     toggle.classList.toggle('active');
- };
+// const toggle = document.getElementById('pet-friendly-toggle');
+// const toggleText = document.getElementById('pet-friendly-text');
 
- // Evento de clique no toggle
- toggle.addEventListener('click', toggleState);
+// // Inicializa uma variável para manter o estado do toggle
+// var isToggleActive = false;
 
- // Evento de clique no texto "Pet friendly"
- toggleText.addEventListener('click', toggleState);
+// // Função para alternar o estado do toggle
+// const toggleState = () => {
+//     toggle.classList.toggle('active');
+//     // Atualiza a variável de estado baseada na presença da classe 'active'
+//     isToggleActive = toggle.classList.contains('active');
+// };
 
+// // Evento de clique no toggle
+// toggle.addEventListener('click', toggleState);
 
- 
+// // Evento de clique no texto "Pet friendly"
+// toggleText.addEventListener('click', toggleState);
+
+// document.getElementById('btnFreeEstimate').addEventListener('click', function() {
+//     const bedrooms = document.getElementById('dropdownBedroom').value;
+//     const bathrooms = document.getElementById('dropdownBathroom').value;
+//     const pets = isToggleActive ? 'yes' : 'no';
+//     const url = `https://flashguyscleaning.com/version-test/schedule?bathroom=${bathrooms}&bedrooms=${bedrooms}&pets=${pets}`;
+  
+//     window.open(url, '_blank'); // Abre a URL em uma nova aba
+    
+//   });
